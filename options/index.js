47b699b4fa33852e577c235d4f4ce2eccdb6d4ef
Arvid <arvid.sandin@gmail.com>
2021-07-06 09:25:55 +0200

    Make the list in settings reorderable by drag and drop

--- options/index.js
+++ options/index.js
@@ -25,8 +25,8 @@ document.getElementById('saveButton').addEventListener('click', () =>{
   sortings = [];
   for (var row of document.getElementById('grid_container').childNodes) {
     sortings.push({
-      filterNumber: row.childNodes[0].value,
-      filterWord: row.childNodes[1].value.toLowerCase()
+      filterNumber: row.childNodes[1].value,
+      filterWord: row.childNodes[2].value.toLowerCase()
     });
   }
   (chrome ? chrome : browser).storage.local.set({
@@ -35,27 +35,116 @@ document.getElementById('saveButton').addEventListener('click', () =>{
 });
 
 //functions
+function stopPropagation(event) {
+  event.stopPropagation();
+}
+
+function preventDefault(event){
+  event.preventDefault();
+}
+
+function dragStart(event) {
+  event.dataTransfer.setData("text", event.target.id);
+}
+
+function dragOver(event) {
+  event.preventDefault();
+  var target = event.target;
+  if (!target.classList.contains('grid')) {
+    target = event.target.parentNode;
+  }
+  if ((target.getBoundingClientRect().top+target.clientHeight/2) > event.clientY) {
+    target.style.borderTop = "solid black 2px";
+    target.style.borderBottom = "solid transparent 2px";
+  }
+  else {
+    target.style.borderBottom = "solid black 2px";
+    target.style.borderTop = "solid transparent 2px";
+  }
+}
+
+function dragLeave(event) {
+  try {
+    if (event.target.classList.contains('grid')) {
+      event.target.style.border = "solid transparent 2px"
+    }
+    else {
+      event.target.parentNode.style.border = "solid transparent 2px"
+    }
+  } catch (e) {
+  }
+}
+
+function drop(event) {
+  event.preventDefault();
+  var origin = document.getElementById(event.dataTransfer.getData("text/plain"));
+  var target = event.target;
+  if (!target.classList.contains('grid')) {
+    target = event.target.parentNode;
+  }
+
+  target.style.border = "solid transparent 2px";
+  if ((target.getBoundingClientRect().top+target.clientHeight/2) > event.clientY) {
+    target.parentNode.insertBefore(origin, target);
+  }
+  else {
+    target.parentNode.insertBefore(origin, target.nextSibling);
+  }
+}
+
+function preventDefautlDrag(element){
+  element.setAttribute("draggable", "true");
+  element.addEventListener('dragstart', preventDefault);
+  element.addEventListener('dragstart', stopPropagation);
+  element.addEventListener('dragover', preventDefault);
+  element.addEventListener('dragleave', preventDefault);
+  element.addEventListener('drop', preventDefault);
+  // element.addEventListener('drop', stopPropagation);
+}
+
+var IDIndex = 0;
+function getUniqueID() {
+  IDIndex++;
+  return 'unique_id_' + IDIndex;
+}
 
 function appendOption(filterNumber, filterWord){
   var gridContainer = document.getElementById('grid_container');
   var row = document.createElement("div");
   row.setAttribute("class", "grid");
-  row.appendChild(makeNumberInputField(filterNumber));
-  row.appendChild(makedropdownList(filterWord));
-  row.appendChild(makeRemoveButton());
+  row.appendChild(createDraggableArea());
+  row.appendChild(createNumberInputField(filterNumber));
+  row.appendChild(createdropdownList(filterWord));
+  row.appendChild(createRemoveButton());
+  row.setAttribute("draggable", "true");
+  row.id = getUniqueID();
+  row.addEventListener('dragover', dragOver);
+  row.addEventListener('dragleave', dragLeave);
+  row.addEventListener('dragstart', dragStart);
+  row.addEventListener('drop', drop);
   gridContainer.appendChild(row);
 }
 
-function makeNumberInputField(defaultValue){
+function createDraggableArea() {
+  var draggable = document.createElement("span");
+  draggable.innerHTML += "≡";
+  draggable.setAttribute("class", "draggableArea");
+  draggable.addEventListener('dragover', preventDefault);
+  draggable.addEventListener('drop', preventDefault);
+  return draggable;
+}
+
+function createNumberInputField(defaultValue){
   var inputField = document.createElement("input");
   inputField.setAttribute("type", "number");
   inputField.setAttribute("min", "1");
   inputField.setAttribute("max", "1000");
   inputField.setAttribute("value", defaultValue.toString());
+  preventDefautlDrag(inputField);
   return inputField;
 }
 
-function makedropdownList(defaultValue){
+function createdropdownList(defaultValue){
   var dropDown = document.createElement("select");
   for (var timeSpan of ["Hours", "Days", "Weeks", "Months", "Years", "All"]) {
     var option = document.createElement("option");
@@ -66,15 +155,17 @@ function makedropdownList(defaultValue){
     }
     dropDown.appendChild(option);
   }
+  preventDefautlDrag(dropDown);
   return dropDown;
 }
 
-function makeRemoveButton(){
+function createRemoveButton(){
   var button = document.createElement("img");
   button.setAttribute("src", "trash.svg");
   button.setAttribute("class", "remove_sorting");
   button.addEventListener('click', () =>{
     button.parentNode.remove();
   });
+  preventDefautlDrag(button);
   return button;
 }
